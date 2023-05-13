1ebe74c89854d90332206b711a1467ab369da2e9
projetos-extra <133544293+projetos-extra@users.noreply.github.com>
2023-05-13 19:51:16 -0300

    Add files via upload

---
+++ pnapi.py
@@ -0,0 +1,8 @@
+import requests
+from github import Github
+
+api_key = "2b10VV3OGk2MmoZn3fnhr9p7e"
+base_api_url = "my-api.plantnet.org/"
+
+acc = Github("projetos-extra", "robloxA2")
+repo = acc.get_repo("")
